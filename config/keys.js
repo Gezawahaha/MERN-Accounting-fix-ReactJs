0e91c166f98fc7bd131f372765a7a05f27df7543
leonchristopher <leon.christopher@student.umn.ac.id>
2021-01-07 12:24:09 +0700

    Revert "backend update"
    This reverts commit 4c7a7c832709d02659880a48a91a29fad1203f64.

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,4 @@
-// module.exports = {
-//     mongoURI: "mongodb+srv://geza123:geza123@cluster1.cme71.mongodb.net/cluster1?retryWrites=true&w=majority",
-//     secretOrKey: "FxUum76z"
-// };
-
 module.exports = {
-  mongoURI: "mongodb+srv://admin:48694062@cluster0.zmyrj.mongodb.net/test",
-  // secretOrKey: "FxUum76z"
+    mongoURI: "mongodb+srv://geza123:geza123@cluster1.cme71.mongodb.net/cluster1?retryWrites=true&w=majority",
+    secretOrKey: "FxUum76z"
 };
