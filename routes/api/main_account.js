2acce5835fdc7b2411fa503bd5ea06a9385541fd
leonchristopher <leon.christopher@student.umn.ac.id>
2021-01-13 21:46:17 +0700

    Update 13 Jan
    1. Fix CoA update_at
    2. CoA debit kredit auto update from Main Acc
    3. Main Acc debit kredit auto update from Sub
    4. Post sub with debit kredit and auto update to Main
    5. Added get MoA-data & Sub-data

--- routes/api/main_account.js
+++ routes/api/main_account.js
@@ -97,4 +97,67 @@ router.delete("/:postId", async (req, res) => {
   }
 });
 
+//GET ALL POST
+router.get("/MoA-data", async (req, res) => {
+  try {
+    // const post = await Post.findById({account_number: req.params.postId});
+    const posts = await Post.find();
+    const datacount = posts.length;
+    let count = 0;
+    Object.keys(posts).map(async (item) => {
+      let temporary = await Sub.find({
+        main_account_number: posts[item].main_account_number,
+      });
+
+      let temporary_totaldebit = posts[item].total_debit;
+      let temporary_totalkredit = posts[item].total_kredit;
+      let total_debit = 0;
+      let total_kredit = 0;
+
+      Object.keys(temporary).map((temporaryitem) => {
+        // Create a new array based on current state:
+        total_debit = total_debit + temporary[temporaryitem].total_debit;
+        total_kredit = total_kredit + temporary[temporaryitem].total_kredit;
+      });
+
+      if (
+        temporary_totaldebit == total_debit &&
+        temporary_totalkredit == total_kredit
+      ) {
+        const updatedpost = await Post.updateOne(
+          {main_account_number: posts[item].main_account_number},
+          {
+            $set: {
+              name: posts[item].name,
+              total_debit: total_debit,
+              total_kredit: total_kredit,
+            },
+          }
+        );
+      } else {
+        const updatedpost = await Post.updateOne(
+          {main_account_number: posts[item].main_account_number},
+          {
+            $set: {
+              name: posts[item].name,
+              total_debit: total_debit,
+              total_kredit: total_kredit,
+              updated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+            },
+          }
+        );
+      }
+
+      count = count + 1;
+
+      if (datacount == count) {
+        const anotherone = await Post.find();
+        res.json(anotherone);
+      }
+    });
+  } catch (err) {
+    res.json({message: err});
+  }
+});
+
 module.exports = router;
--- routes/api/sub_account.js
+++ routes/api/sub_account.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../../models/sub_account");
+const Main = require("../../models/main_account");
 const moment = require("moment");
 
-//GET ALL THE POST
-// router.get("/", async (req, res) => {
-//   try {
-//     const posts = await Post.find();
-//     res.json(posts);
-//   } catch (err) {
-//     res.json({message: err});
-//   }
-// });
+//GET ALL SUB ACCOUNT
+router.get("/Sub-data", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({message: err});
+  }
+});
 
-//SUBMIT A POST
+//CREATE SUB ACCOUNT / INPUT PENGELUARAN
 router.post("/", async (req, res) => {
   const post = new Post({
     sub_account_number: req.body.sub_account_number,
     name: req.body.name,
-    total_debit: 0,
-    total_kredit: 0,
+    total_debit: req.body.total_debit,
+    total_kredit: req.body.total_kredit,
     created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
     updated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
     main_account_number: req.body.main_account_number,
@@ -27,13 +28,33 @@ router.post("/", async (req, res) => {
 
   try {
     const savedPost = await post.save();
+    try {
+      const specific_main_account = await Main.findOne({
+        main_account_number: req.body.main_account_number,
+      });
+      let debit = specific_main_account.total_debit;
+      let kredit = specific_main_account.total_kredit;
+      const updatedpost = await Main.updateOne(
+        {main_account_number: req.body.main_account_number},
+        {
+          $set: {
+            name: req.body.name,
+            total_debit: debit + req.body.total_debit,
+            total_kredit: kredit + req.body.total_kredit,
+            updated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+          },
+        }
+      );
+    } catch (err) {
+      res.json({message: err});
+    }
     res.json(savedPost);
   } catch (err) {
     res.json({message: err});
   }
 });
 
-//SPECIFIC POST
+//GET LIST SUB ACCOUNT BY MAIN ACCOUNT NUMBER
 router.get("/:postId", async (req, res) => {
   try {
     // const post = await Post.findById({account_number: req.params.postId});
@@ -44,7 +65,7 @@ router.get("/:postId", async (req, res) => {
   }
 });
 
-//UPDATE POST
+//EDIT PENGELUARAN
 router.patch("/:postId", async (req, res) => {
   try {
     const updatedpost = await Post.updateOne(
@@ -58,6 +79,7 @@ router.patch("/:postId", async (req, res) => {
         },
       }
     );
+
     res.json(updatedpost);
   } catch (err) {
     res.json({message: err});
